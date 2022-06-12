9fa9f5abac22e7628cfc15cf88f24bd2b4ebf7c6
Aniket Kulkarni <aniket16kulkarni@gmail.com>
2022-06-12 14:40:02 +0200

    Sorting - Bubble Sort Easy

---
+++ sorting/src/com/java/problem/easy/BubbleSort.java
@@ -0,0 +1,20 @@
+package com.java.problem.easy;
+
+public class BubbleSort {
+
+    // Best: O(n) time | O(1) space
+    // Average: O(n^2) time | O(1) space
+    // Worst: O(n^2) time | O(1) space
+    public static int[] bubbleSort(int[] array) {
+        for (int outer = 0; outer < array.length; outer++) {
+            for (int inner = 1; inner < array.length - outer; inner++) {
+                if (array[inner - 1] > array[inner]) {
+                    int temp = array[inner - 1];
+                    array[inner - 1] = array[inner];
+                    array[inner] = temp;
+                }
+            }
+        }
+        return array;
+    }
+}
