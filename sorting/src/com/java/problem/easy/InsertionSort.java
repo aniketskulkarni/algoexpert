7c11d4d380888b227a09d199d8111efb1b8a4d57
Aniket Kulkarni <aniket16kulkarni@gmail.com>
2022-06-12 15:27:57 +0200

    Sorting - Insertion Sort Easy

---
+++ sorting/src/com/java/problem/easy/InsertionSort.java
@@ -0,0 +1,20 @@
+package com.java.problem.easy;
+
+public class InsertionSort {
+
+    // Best: O(n) time | O(1) space
+    // Average: O(n^2) time | O(1) space
+    // Worst: O(n^2) time | O(1) space
+    public static int[] insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (array[j - 1] > array[j]) {
+                    int temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
+}
